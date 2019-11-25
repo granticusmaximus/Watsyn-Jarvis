3cf06268dc2cf3c425cac1817c1c75baa4e0ed57
granticusmaximus <gwatson117@gmail.com>
2019-11-25 13:23:42 -0500

    Removal of Files

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import "materialize-css/dist/css/materialize.min.css";
 import "materialize-css/dist/js/materialize.min.js";
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
-import reduxThunk from 'redux-thunk';
-import reducers from './reducers';
+//import {Provider} from 'react-redux';
+//import {createStore, applyMiddleware} from 'redux';
+//import reduxThunk from 'redux-thunk';
+//import reducers from './reducers';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import Firebase, { FirebaseContext } from './components/Firebase';
 
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+/*const store = createStore(reducers, {}, applyMiddleware(reduxThunk));*/
 
 ReactDOM.render(
   <FirebaseContext.Provider value={new Firebase()}>
