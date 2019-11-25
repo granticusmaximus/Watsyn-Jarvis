606d93defc7a31fa5a72f6a4d761aeb4ca917789
granticusmaximus <gwatson117@gmail.com>
2019-11-25 13:58:46 -0500

    Additional updates to css

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,9 @@ import "../assets/css/App.css";
 
 const Home = () => (
   <div className="container">
-    <center><h1 className="bgf">Welcome</h1></center>
-    <p>Welcome to Jarvis. This is application is a workflow application built for engineers by engineers</p>
+    <center><h1 className="bgf">Welcome to Jarvis.</h1></center>
+    <h3>This is application is a workflow application built for engineers by engineers</h3>
+    <p>This application has a note taking application, and future iterations of this application will have a tasking board as well as a git repositiory to load projects</p>
   </div>
 );
 
