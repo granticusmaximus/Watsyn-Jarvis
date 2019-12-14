9a287a925d338be760d7b2c2cf7bbe4e575fd8eb
Grant Watson <gwatson117@gmail.com>
2019-12-14 15:51:57 -0700

    Refactoring how client info is entered

--- src/components/ClientData/Client.js
+++ src/components/ClientData/Client.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import ClientForm from './clientForm';
 import ClientList from './clientList';
 
 class Client extends Component {
   render() {
     return (
       <div className="container">
-        <ClientForm />
         <ClientList />
       </div>
     );
