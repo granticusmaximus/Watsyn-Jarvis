89e85a4a8be96d83f28a1545eab029c74cf509ee
Grant Watson <gwatson117@gmail.com>
2019-12-24 15:07:52 -0700

    Update ViewClient.js

--- src/components/ClientData/ViewClient.js
+++ src/components/ClientData/ViewClient.js
@@ -4,3 +4,28 @@ import {
     ModalBody,
     ModalFooter
 } from "reactstrap";
+
+class ViewClient extends Component {
+    constructor() {
+        super();
+        this.state = {
+            listName: "",
+            cardName: "",
+            editCardName: "",
+            keyCard: "",
+            keyList: "",
+            inputKey: "",
+            clientList: [],
+            editing: false
+        };
+    }
+    render() {
+        return (
+            <div className="container">
+
+            </div>
+        );
+    }
+}
+
+export default Data;
